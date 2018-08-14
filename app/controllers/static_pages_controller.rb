c3b35252e6817de8c350308d14d0654f03eaae98
EC2 Default User <ec2-user@ip-172-31-43-55.us-east-2.compute.internal>
2018-08-14 06:14:01 +0000

    ajax処理追加中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,17 @@ class StaticPagesController < ApplicationController
     puts "実行"
     puts a
   end
+  
+  def test
+    # rubyファイルの実行
+    a = `ruby test.rb`
+    # %x( "echo 'hello world'" )
+    puts "ボタンで実行された"
+    puts a
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
